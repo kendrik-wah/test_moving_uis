a944924a96b9de3c478f3acdfc918e2cd293b9ec
kendrik-wah <kendrik_wah@yahoo.com.sg>
2020-08-03 02:31:55 +0800

    Updates

---
+++ precache-manifest.862d6c766c045c74d79592c834222df9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60b97e679f1f80a0ad66879d9642336",
+    "url": "/test_moving_uis/index.html"
+  },
+  {
+    "revision": "65d1e44b494d835f2393",
+    "url": "/test_moving_uis/static/css/main.fde4dfca.chunk.css"
+  },
+  {
+    "revision": "3563cb0551b7c5ce79cd",
+    "url": "/test_moving_uis/static/js/2.1520aabd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test_moving_uis/static/js/2.1520aabd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65d1e44b494d835f2393",
+    "url": "/test_moving_uis/static/js/main.ffaccd5c.chunk.js"
+  },
+  {
+    "revision": "ca78fd5c17a8f571d21d",
+    "url": "/test_moving_uis/static/js/runtime-main.5119c561.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_moving_uis/static/media/logo.5d5d9eef.svg"
+  }
+]);
